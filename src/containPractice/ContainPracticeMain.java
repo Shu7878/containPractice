5e2dcc209ffcf45263ebb231735c85eb3b3b4e58
shu-g <kksasasa1177@gmail.com>
2021-01-10 21:11:56 +0900

    更新4回目
    各文字列を区切ることができるように設定を変更しました。

--- src/containPractice/ContainPracticeMain.java
+++ src/containPractice/ContainPracticeMain.java
@@ -27,6 +27,7 @@ public class ContainPracticeMain {
 		*/
 		/* ------------------------- */
 
+		/*
 		//ArrayListを用いて列ごとに判断したい場合
 		System.out.println("列の個数を整数値で入力してください");
 		int countNum = sc.nextInt();
@@ -63,6 +64,65 @@ public class ContainPracticeMain {
 		//含まれている個数の出力
 		for(int i = 0; i < column.size(); i++){
 			System.out.println(countColumn.get(i));
+		}
+
+		*/
+
+
+		//ArrayListを用いてエリアごとに判断したい場合
+		System.out.println("列の個数を整数値で入力してください");
+		int countNum = sc.nextInt();
+
+		List<String> column = new ArrayList<String>();
+		List<Integer> countColumn = new ArrayList<Integer>();
+
+		//検索したい文字の設定
+		System.out.println("検索したい文字を入力してください");
+		String search = sc.next();
+
+
+		//文字列の設定
+
+
+		System.out.println("先ほど入力した文字が含まれているか検索したい文字列を "+ countNum + " 回入力してください");
+		for(int i = 0; i < countNum; i++){
+			System.out.println("文字列を入力してください");
+			String newLine = sc.next();
+			System.out.println("何文字目までで区切りをつけたいかを入力してください");
+			int sepNum = sc.nextInt();
+			String firstHalf = newLine.substring(0, sepNum);
+			String secondHalf = newLine.substring(sepNum);
+			column.add(firstHalf);
+			column.add(secondHalf);
+
+		}
+
+
+
+		//回数を数えるカウントの設定
+		for(int i = 0; i < column.size()*2; i++){
+			countColumn.add(0);
+		}
+
+		//出てきた回数を記録
+		for(int i = 0; i < column.size(); i++){
+			while(true){
+				index = column.get(i).indexOf(search, index) + 1;
+				if(index == 0){
+					break;
+				}
+				countColumn.set(i, countColumn.get(i) + 1);
+			}
+		}
+
+		//前半後半の文字列と含まれている個数の出力
+		for(int i = 0; i < countNum*2; i++){
+			if(i % 2 == 0){
+				System.out.println( "前半：" + column.get(i));
+			}else{
+				System.out.println( "後半:" + column.get(i));
+			}
+			System.out.println(search + "が含まれている個数：" + countColumn.get(i) + "個");
 		}
 
 	}
